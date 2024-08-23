565fb49c881b82508c5fe3f36c0b71e75926f55c
不争 <125673111+ppbghj@users.noreply.github.com>
2024-08-23 14:31:19 +0800

    Update 连点器.py

--- 连点器.py
+++ 连点器.py
@@ -1,10 +1,12 @@
 from pynput import mouse
 from pynput import keyboard
 import time
-f=float(input('please input click frequency'))
+times=int(input('input frequency/60'))
+print('{0} times per seceond'.format(str(times*62.5)))
 control=mouse.Controller()
 def speed_monitor():
     position1=control.position
+    time.sleep(0.0001)
     position2=control.position
     speed=((position1[0]-position2[0])**2+(position1[1]-position2[1])**2)**0.5/0.00001
     if speed==0:
@@ -15,10 +17,9 @@ def speed_monitor():
 def main():
     while 1:
         if speed_monitor():
-            control.click(mouse.Button.left,1)
+            control.click(mouse.Button.left,times)
         else:
             print('stop process')
-            time.sleep((1/f)-0.006)
             return None
 
 def key_press(key):
@@ -27,4 +28,3 @@ def key_press(key):
 with keyboard.Listener(on_press=key_press) as listener:
     listener.join()
 main()
-    
